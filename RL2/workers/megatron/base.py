1db99312179c304c9a274d7bee6b7c70934d4c33
ChenmienTan <chenmientan@outlook.com>
2025-11-04 06:43:36 +0000

    fix bug of assigning dtype for MegatronWorker

--- RL2/workers/megatron/base.py
+++ RL2/workers/megatron/base.py
@@ -40,11 +40,9 @@ class MegatronWorker(Worker):
         self.bridge = AutoBridge.from_hf_pretrained(config.model_name)
 
         self.provider = self.bridge.to_megatron_provider()
-        setattr(
-            self.provider,
-            {"float16": "fp16", "bfloat16": "bf16"}[config.dtype],
-            True
-        )
+        assert config.dtype in ["float16", "bfloat16"], "Only `float16` and `bfloat16` is supported."
+        if config.dtype == "float16":
+            self.provider.fp16, self.provider.bf16 = True, False
         self.provider.attention_backend = "flash"
         tf_config = OmegaConf.to_container(config.tf_config)
         for k, v in tf_config.items():
