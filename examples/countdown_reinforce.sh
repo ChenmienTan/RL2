1fce214610f6222af2983a2cc9f5f04ecfb96d4b
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-11-12 00:25:40 +0800

    update example to adapt updated config

--- examples/countdown_reinforce.sh
+++ examples/countdown_reinforce.sh
@@ -6,6 +6,7 @@ torchrun \
     train_data.responses_per_prompt=4 \
     train_data.sampling_params.max_new_tokens=1024 \
     "train_data.sampling_params.stop=['</answer>']" \
+    train_data.apply_chat_template=false \
     test_data.path=test@Chenmien/Countdown \
     actor.model_name=Qwen/Qwen2.5-3B-Instruct \
     actor.max_length_per_device=8192 \
