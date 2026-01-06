889b21f6846ac173b5e630f894657facdc514b23
米晨 <tanchenmien.tcme@alibaba-inc.com>
2026-01-06 17:47:30 +0800

    update example to adapt the updated config of SFT

--- examples/limo_sft.sh
+++ examples/limo_sft.sh
@@ -1,9 +1,9 @@
 torchrun \
     --nproc_per_node=4 \
     -m RL2.trainer.sft \
-    data.path=Chenmien/LIMO \
-    data.max_length=16384 \
-    data.batch_size=32 \
+    data.train.path=Chenmien/LIMO \
+    data.train.max_length=16384 \
+    data.train.batch_size=32 \
     actor.model_name=Qwen/Qwen2.5-7B-Instruct \
     actor.cp_size=4 \
     actor.max_length_per_device=4096 \
